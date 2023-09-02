18e66e1b81d5be5db47d926d212795c4865bd39d
shunakid <shunakid@icloud.com>
2023-09-02 21:27:26 +0900

    Clerkを使用した認証の追加作業

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+const DashboardPage = () => {
+  return (
+    <div>
+      <h1>Dashboard Page ( 保護されたページ )</h1>
+    </div>
+  );
+};
+
+export default DashboardPage;
---
+++ app/(landing)/page.tsx
@@ -0,0 +1,9 @@
+const LandingPage = () => {
+  return (
+    <div>
+      <h1>Landing Page ( 保護されていないページ )</h1>
+    </div>
+  );
+};
+
+export default LandingPage;
