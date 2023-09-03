a9a3e7a426ffda125fed601268ea3798e741440b
shunakid <shunakid@icloud.com>
2023-09-03 10:58:29 +0900

    Clerkを使用した認証の追加作業

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,9 +1,11 @@
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
 const DashboardPage = () => {
   return (
     <div>
       <h1>Dashboard Page ( 保護されたページ )</h1>
+      <UserButton afterSignOutUrl="/" />
     </div>
   );
 };
--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,7 +1,18 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 const LandingPage = () => {
   return (
     <div>
       <h1>Landing Page ( 保護されていないページ )</h1>
+      <div>
+        <Button>
+          <Link href="/sign-in">ログイン</Link>
+        </Button>
+        <Button>
+          <Link href="/sign-up">新規登録</Link>
+        </Button>
+      </div>
     </div>
   );
 };
