218acc91efd6c118b1d21b1de2c7fb85b19bacae
shunakid <shunakid@icloud.com>
2023-09-10 16:42:03 +0900

    サイドバー一部をコンポーネントに切り出し

---
+++ components/mobile-sidebar.tsx
@@ -0,0 +1,16 @@
+"use cliant";
+
+import { Menu } from "lucide-react";
+import { Button } from "./ui/button";
+
+const MobileSidebar = () => {
+  return (
+    <div>
+      <Button variant="ghost" size="icon" className=" md:hidden">
+        <Menu />
+      </Button>
+    </div>
+  );
+};
+
+export default MobileSidebar;
