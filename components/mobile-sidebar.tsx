e3b767d6c9574e520f18a522a44940f705d0a968
shunakid <shunakid@icloud.com>
2023-09-10 16:59:00 +0900

    shadcnを使ったサイドバーメニューの表示・非表示機能の追加作業

--- components/mobile-sidebar.tsx
+++ components/mobile-sidebar.tsx
@@ -2,13 +2,22 @@
 
 import { Menu } from "lucide-react";
 import { Button } from "./ui/button";
+import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
+import Sidebar from "./sidebar";
 
 const MobileSidebar = () => {
   return (
     <div>
-      <Button variant="ghost" size="icon" className=" md:hidden">
-        <Menu />
-      </Button>
+      <Sheet>
+        <SheetTrigger>
+          <Button variant="ghost" size="icon" className=" md:hidden">
+            <Menu />
+          </Button>
+        </SheetTrigger>
+        <SheetContent side="left" className="p-0">
+          <Sidebar />
+        </SheetContent>
+      </Sheet>
     </div>
   );
 };
