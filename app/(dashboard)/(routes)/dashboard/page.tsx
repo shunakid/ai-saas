c899d0368c633482e61fbaf29fd688addb87ece3
shunakid <shunakid@icloud.com>
2023-09-10 09:36:34 +0900

    サイドバー基本形完成

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,11 +1,7 @@
-import { Button } from "@/components/ui/button";
-import { UserButton } from "@clerk/nextjs";
-
 const DashboardPage = () => {
   return (
     <div>
       <h1>Dashboard Page ( 保護されたページ )</h1>
-      <UserButton afterSignOutUrl="/" />
     </div>
   );
 };
