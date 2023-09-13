30f8467df29a4902080e82a893fbf59d5973a4a7
shunakid <shunakid@icloud.com>
2023-09-14 01:17:30 +0900

    ダッシュボード画面作成作業

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,7 +1,81 @@
+"use client";
+
+import { Card } from "@/components/ui/card";
+import { cn } from "@/lib/utils";
+import {
+  ArrowRight,
+  Code,
+  ImageIcon,
+  MessageSquare,
+  Music,
+  VideoIcon,
+} from "lucide-react";
+import { useRouter } from "next/navigation";
+
+const tools = [
+  {
+    label: "Convesation",
+    icon: MessageSquare,
+    color: "text-violet-500",
+    bgColor: "bg-violet-500/10",
+    href: "/conversation",
+  },
+  {
+    label: "Music Generator",
+    icon: Music,
+    color: "text-emerald-500",
+    bgColor: "bg-emerald-500/10",
+    href: "/music",
+  },
+  {
+    label: "Image Generator",
+    icon: ImageIcon,
+    color: "text-pink-700",
+    bgColor: "bg-pink-700/10",
+    href: "/image",
+  },
+  {
+    label: "Video Generator",
+    icon: VideoIcon,
+    color: "text-orange-700",
+    bgColor: "bg-orange-700/10",
+    href: "/video",
+  },
+  {
+    label: "Code Generator",
+    icon: Code,
+    color: "text-green-700",
+    bgColor: "bg-green-700/10",
+    href: "/code",
+  },
+];
+
 const DashboardPage = () => {
+  const router = useRouter();
   return (
     <div>
-      <h1>Dashboard Page ( 保護されたページ )</h1>
+      <div className="mb-8 space-y-4">
+        <h2 className="text-center text-2xl font-bold md:text-4xl">
+          Explor the power of AI
+        </h2>
+      </div>
+      <div className="space-y-4 px-4 md:px-20 lg:px-32">
+        {tools.map((tool) => (
+          <Card
+            onClick={() => router.push(tool.href)}
+            key={tool.href}
+            className="flex cursor-pointer items-center justify-between border-black/5 p-4 transition hover:shadow-md"
+          >
+            <div className="flex items-center gap-x-4">
+              <div className={cn("w-fit rounded-md p-2", tool.bgColor)}>
+                <tool.icon className={cn("h-8 w-8", tool.color)} />
+              </div>
+              <div className="font-semibold">{tool.label}</div>
+            </div>
+            <ArrowRight className="h-5 w-5" />
+          </Card>
+        ))}
+      </div>
     </div>
   );
 };
