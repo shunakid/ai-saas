89affe29b2e7f8853f759e28e5c22689dcbd5290
shunakid <shunakid@icloud.com>
2023-09-24 19:41:29 +0900

    fix: MobileSidebarにおけるネストされたbutton要素の警告を解消
    ButtonコンポーネントがSheetTriggerコンポーネントの内部にネストされていたため、両コンポーネントが<button>要素をレンダリングし、validateDOMNestingの警告が発生していました。ButtonコンポーネントにasChildプロパティをtrueに設定することで、これがSlotコンポーネントとしてレンダリングされるようになり、警告が解消されました。

--- components/mobile-sidebar.tsx
+++ components/mobile-sidebar.tsx
@@ -23,7 +23,7 @@ const MobileSidebar = () => {
     <div>
       <Sheet>
         <SheetTrigger>
-          <Button variant="ghost" size="icon" className=" md:hidden">
+          <Button asChild variant="ghost" size="icon" className=" md:hidden">
             <Menu />
           </Button>
         </SheetTrigger>
