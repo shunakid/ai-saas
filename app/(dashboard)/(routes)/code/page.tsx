8fa4850e0a46c5833adf16db4d971336bdf5cb63
shunakid <shunakid@icloud.com>
2023-09-30 22:57:43 +0900

    feat(CodePage): コード生成機能の包括的な実装 - formのバリデーションのためのzodスキーマを追加 - コード生成のためのCodePageコンポーネントを作成 - react-hook-formを使用したformのハンドリングを実装 - メッセージのステート管理を実装 - ローディング状態を示すLoaderコンポーネントを追加 - UserAvatarとBotAvatarコンポーネントを追加 - ReactMarkdownを使用したmarkdownのレンダリングを実装 - formおよび他のUIコンポーネントのスタイリングを改善 - form送信後のルーターリフレッシュを追加 - 新機能およびコンポーネントのドキュメントを更新

---
+++ app/(dashboard)/(routes)/code/page.tsx
@@ -0,0 +1,154 @@
+"use client";
+
+import * as z from "zod";
+import axios from "axios";
+import { Code } from "lucide-react";
+import { useForm } from "react-hook-form";
+import { useState } from "react";
+import ReactMarkdown from "react-markdown";
+import { useRouter } from "next/navigation";
+import { ChatCompletionRequestMessage } from "openai";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import { BotAvatar } from "@/components/bot-avatar";
+import { Heading } from "@/components/heading";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
+import { cn } from "@/lib/utils";
+import { Loader } from "@/components/loader";
+import { UserAvatar } from "@/components/user-avatar";
+import { Empty } from "@/components/empty";
+
+import { formSchema } from "./constants";
+
+const CodePage = () => {
+  const router = useRouter();
+  const [messages, setMessages] = useState<ChatCompletionRequestMessage[]>([]);
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      prompt: "",
+    },
+  });
+
+  const isLoading = form.formState.isSubmitting;
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      const userMessage: ChatCompletionRequestMessage = {
+        role: "user",
+        content: values.prompt,
+      };
+      const newMessages = [...messages, userMessage];
+
+      const response = await axios.post("/api/code", { messages: newMessages });
+      setMessages((current) => [...current, userMessage, response.data]);
+
+      form.reset();
+    } catch (error: any) {
+    } finally {
+      router.refresh();
+    }
+  };
+
+  return (
+    <div>
+      <Heading
+        title="Code Generation"
+        description="Generate code using descriptive text."
+        icon={Code}
+        iconColor="text-green-700"
+        bgColor="bg-green-700/10"
+      />
+      <div className="px-4 lg:px-8">
+        <div>
+          <Form {...form}>
+            <form
+              onSubmit={form.handleSubmit(onSubmit)}
+              className="
+                grid
+                w-full
+                grid-cols-12
+                gap-2
+                rounded-lg
+                border
+                p-4
+                px-3
+                focus-within:shadow-sm
+                md:px-6
+              "
+            >
+              <FormField
+                name="prompt"
+                render={({ field }) => (
+                  <FormItem className="col-span-12 lg:col-span-10">
+                    <FormControl className="m-0 p-0">
+                      <Input
+                        className="border-0 outline-none focus-visible:ring-0 focus-visible:ring-transparent"
+                        disabled={isLoading}
+                        placeholder="Simple toggle button using react hooks."
+                        {...field}
+                      />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+              <Button
+                className="col-span-12 w-full lg:col-span-2"
+                type="submit"
+                disabled={isLoading}
+                size="icon"
+              >
+                Generate
+              </Button>
+            </form>
+          </Form>
+        </div>
+        <div className="mt-4 space-y-4">
+          {isLoading && (
+            <div className="flex w-full items-center justify-center rounded-lg bg-muted p-8">
+              <Loader />
+            </div>
+          )}
+          {messages.length === 0 && !isLoading && (
+            <Empty label="No conversation started." />
+          )}
+          <div className="flex flex-col-reverse gap-y-4">
+            {messages.map((message) => (
+              <div
+                key={message.content}
+                className={cn(
+                  "flex w-full items-start gap-x-8 rounded-lg p-8",
+                  message.role === "user"
+                    ? "border border-black/10 bg-white"
+                    : "bg-muted",
+                )}
+              >
+                {message.role === "user" ? <UserAvatar /> : <BotAvatar />}
+                <ReactMarkdown
+                  components={{
+                    pre: ({ node, ...props }) => (
+                      <div className="my-2 w-full overflow-auto rounded-lg bg-black/10 p-2">
+                        <pre {...props} />
+                      </div>
+                    ),
+                    code: ({ node, ...props }) => (
+                      <code className="rounded-lg bg-black/10 p-1" {...props} />
+                    ),
+                  }}
+                  className="overflow-hidden text-sm leading-7"
+                >
+                  {message.content || ""}
+                </ReactMarkdown>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CodePage;
