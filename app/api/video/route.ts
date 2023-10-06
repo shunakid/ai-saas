25ddeab563276b2b5fc6575c43e70f86e34f67b1
shunakid <shunakid@icloud.com>
2023-10-06 20:34:28 +0900

    feat(video): ビデオ関連の新規ファイルを追加
    - `video/constants.ts` を追加: zodを使用してフォームのスキーマを定義。
    - `video/page.tsx` を追加: ビデオ生成ページを作成。ユーザーの入力を受け取り、APIを呼び出してビデオを生成。
    - `api/video/route.ts` を追加: POSTリクエストを処理し、ReplicateAIConfigを使用してビデオを生成。

---
+++ app/api/video/route.ts
@@ -0,0 +1,30 @@
+import { NextResponse } from "next/server";
+
+import { handleRequest } from "../utils/requestHandler";
+import { handleErrors } from "../utils/errorHandling";
+import { replicateAIConfiguration } from "../utils/ReplicateAIConfig";
+
+export async function POST(req: Request): Promise<NextResponse> {
+  try {
+    const { userId, body } = await handleRequest(req);
+    const { prompt } = body;
+
+    const errorResponse = handleErrors({ userId, prompt });
+    if (errorResponse) return errorResponse;
+
+    const response = await replicateAIConfiguration.run(
+      "lucataco/animate-diff:beecf59c4aee8d81bf04f0381033dfa10dc16e845b4ae00d281e2fa377e48a9f",
+      {
+        input: {
+          motion_module: "mm_sd_v14",
+          prompt: prompt,
+        },
+      },
+    );
+
+    return NextResponse.json(response);
+  } catch (error) {
+    console.log("[VIDEO_ERROR]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
