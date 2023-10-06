7b5545af9c242e84738416cbfbd6f6137cf779d6
shunakid <shunakid@icloud.com>
2023-10-06 20:23:27 +0900

    feat(music): 新しい音楽APIのルートを追加 - `POST`関数を定義: リクエストの処理とReplicateAIの設定の実行 - エラーハンドリング機能を追加 - ReplicateAIの設定を利用してレスポンスを返す機能を追加

---
+++ app/api/music/route.ts
@@ -0,0 +1,29 @@
+import { NextResponse } from "next/server";
+
+import { handleRequest } from "../utils/requestHandler";
+import { handleErrors } from "../utils/errorHandling";
+import { replicateAIConfiguration } from "../utils/ReplicateAIConfig";
+
+export async function POST(req: Request): Promise<NextResponse> {
+  try {
+    const { userId, body } = await handleRequest(req);
+    const { prompt } = body;
+
+    const errorResponse = handleErrors({ userId, prompt });
+    if (errorResponse) return errorResponse;
+
+    const response = await replicateAIConfiguration.run(
+      "riffusion/riffusion:8cf61ea6c56afd61d8f5b9ffd14d7c216c0a93844ce2d82ac1c9ecc9c7f24e05",
+      {
+        input: {
+          prompt_a: prompt,
+        },
+      },
+    );
+
+    return NextResponse.json(response);
+  } catch (error) {
+    console.log("[MUSIC_ERROR]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
