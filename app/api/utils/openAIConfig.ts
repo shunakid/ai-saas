7ab6afc8189ffbdabca9ac110d9abd129a3feedf
shunakid <shunakid@icloud.com>
2023-10-06 20:09:50 +0900

    feat(errorHandling): パラメータの検証をZodスキーマでリファクタリング docs(openAIConfig): OpenAI APIの設定とインスタンスのJSDocコメントを追加 docs(requestHandler): handleRequest関数のJSDocコメントを追加

--- app/api/utils/openAIConfig.ts
+++ app/api/utils/openAIConfig.ts
@@ -1,10 +1,26 @@
-// openAIConfig.js
 import { Configuration, OpenAIApi } from "openai";
 
+/**
+ * OpenAI APIの設定オブジェクトを作成します。
+ * 環境変数から OPENAI_API_KEY を取得し、
+ * apiKey プロパティとして設定します。
+ *
+ * @typedef {Object} openAIConfiguration
+ * @property {string} apiKey - OpenAI APIのAPIキー。
+ */
 const openAIConfiguration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+  apiKey: process.env.OPENAI_API_KEY, // 環境変数からAPIキーを取得します。
 });
 
+/**
+ * OpenAIApi インスタンスを作成します。
+ * 上で作成した openAIConfiguration オブジェクトを使用します。
+ *
+ * @typedef {Object} openAIApiInstance
+ * @example
+ * import { openAIApiInstance } from 'このモジュールのパス';
+ * const response = await openAIApiInstance.createCompletion({ ... });
+ */
 const openAIApiInstance = new OpenAIApi(openAIConfiguration);
 
 export { openAIConfiguration, openAIApiInstance };
--- app/api/utils/requestHandler.ts
+++ app/api/utils/requestHandler.ts
@@ -1,9 +1,26 @@
 import { auth as authenticateUser } from "@clerk/nextjs";
 
+/**
+ * handleRequest 関数は、ユーザーの認証を行い、リクエストからJSONボディを抽出します。
+ *
+ * @async
+ * @function
+ * @param {Request} req - 受け取るリクエストオブジェクト。
+ * @returns {Promise<{ userId: string, body: any }>} - ユーザーIDとJSONボディを含むオブジェクトを返すPromise。
+ *
+ * @throws エラーが発生した場合、エラーがスローされます。
+ *
+ * @example
+ * const { userId, body } = await handleRequest(req);
+ */
 async function handleRequest(req: Request) {
+  // ユーザーを認証し、userIdを取得します。
   const { userId } = authenticateUser();
+
+  // リクエストからJSONボディを抽出します。
   const body = await req.json();
 
+  // userIdとbodyを含むオブジェクトを返します。
   return { userId, body };
 }
 
