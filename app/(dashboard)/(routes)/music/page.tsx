5967b9c0ee56aad6ac9f7e40999fdcfe4e7348a3
shunakid <shunakid@icloud.com>
2023-10-06 20:28:44 +0900

    feat(music): 音楽生成ページと関連定数の追加
    - `constants.ts` にフォームのバリデーションスキーマを追加
    - `page.tsx` に音楽生成のページコンポーネントを追加
      - ユーザーの入力を受け取り、音楽を生成する機能を実装
      - 音楽生成中のローディング表示、エラーハンドリング、音楽の再生機能を追加

---
+++ app/(dashboard)/(routes)/music/page.tsx
@@ -0,0 +1,119 @@
+"use client";
+
+import * as z from "zod";
+import axios from "axios";
+import { useState } from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { useRouter } from "next/navigation";
+import { Music } from "lucide-react";
+
+import { Heading } from "@/components/heading";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
+import { Loader } from "@/components/loader";
+import { Empty } from "@/components/empty";
+
+import { formSchema } from "./constants";
+
+const MusicPage = () => {
+  const router = useRouter();
+  const [music, setMusic] = useState<string>();
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      prompt: "",
+    },
+  });
+
+  const isLoading = form.formState.isSubmitting;
+
+  const onSubmit = async (
+    userInput: z.infer<typeof formSchema>,
+  ): Promise<void> => {
+    try {
+      setMusic(undefined);
+
+      const response = await axios.post("/api/music", userInput);
+
+      console.log("Response Data:", response.data);
+
+      setMusic(response.data.audio);
+
+      form.reset();
+    } catch (error: unknown) {
+    } finally {
+      router.refresh();
+    }
+  };
+
+  return (
+    <div>
+      <Heading
+        title="Music Generation"
+        description="Turn your prompt into music."
+        icon={Music}
+        iconColor="text-emerald-500"
+        bgColor="bg-emerald-500/10"
+      />
+      <div className="px-4 lg:px-8">
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="
+              grid
+              w-full
+              grid-cols-12
+              gap-2
+              rounded-lg
+              border
+              p-4
+              px-3
+              focus-within:shadow-sm
+              md:px-6
+            "
+          >
+            <FormField
+              name="prompt"
+              render={({ field }) => (
+                <FormItem className="col-span-12 lg:col-span-10">
+                  <FormControl className="m-0 p-0">
+                    <Input
+                      className="border-0 outline-none focus-visible:ring-0 focus-visible:ring-transparent"
+                      disabled={isLoading}
+                      placeholder="Piano solo"
+                      {...field}
+                    />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+            <Button
+              className="col-span-12 w-full lg:col-span-2"
+              type="submit"
+              disabled={isLoading}
+              size="icon"
+            >
+              Generate
+            </Button>
+          </form>
+        </Form>
+        {isLoading && (
+          <div className="p-20">
+            <Loader />
+          </div>
+        )}
+        {!music && !isLoading && <Empty label="No music generated." />}
+        {music && (
+          <audio controls className="mt-8 w-full">
+            <source src={music} />
+          </audio>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default MusicPage;
