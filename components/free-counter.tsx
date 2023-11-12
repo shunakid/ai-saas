54851675ccca16e5969d938465e7ab8be5f618f9
shunakid <shunakid@icloud.com>
2023-11-12 16:32:55 +0900

    feat(free-counter): Proユーザー向け機能をFreeCounterに追加
    free-counter.tsxにisProプロパティとuseProModalフックを追加。
    ProモーダルのトリガーとしてButtonにonClickイベントを統合。
    Proユーザーの場合、FreeCounterコンポーネントを非表示にするロジックを実装。
    カウンターの進捗バーとアップグレードボタンを更新。

--- components/free-counter.tsx
+++ components/free-counter.tsx
@@ -5,19 +5,27 @@ import { MAX_FREE_COUNTS } from "@/constants";
 import { Card, CardContent } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { Progress } from "@/components/ui/progress";
+import { useProModal } from "@/hooks/use-pro-modal";
 
 export const FreeCounter = ({
+  isPro = false,
   apiLimitCount = 0,
 }: {
+  isPro: boolean;
   apiLimitCount: number;
 }) => {
   const [mounted, setMounted] = useState(false);
+  const proModal = useProModal();
 
   useEffect(() => {
     setMounted(true);
   }, []);
 
   if (!mounted) {
+    return null;
+  }
+
+  if (isPro) {
     return null;
   }
 
@@ -34,7 +42,11 @@ export const FreeCounter = ({
               value={(apiLimitCount / MAX_FREE_COUNTS) * 100}
             />
           </div>
-          <Button className="w-full" variant="premium">
+          <Button
+            onClick={proModal.onOpen}
+            variant="premium"
+            className="w-full"
+          >
             Upgrade
             <Zap className="ml-2 h-4 w-4 fill-white" />
           </Button>
